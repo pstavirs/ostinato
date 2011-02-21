16d85a338aa0642c86316407f88f0ff30b1c3e83
Srivats P. <pstavirs@gmail.com>
2011-02-21 22:17:16 +0530

    snapshot before 2 pass changes

---
+++ test/main.cpp
@@ -0,0 +1,42 @@
+
+#include "pcapfileformat.h"
+#include "pdmlfileformat.h"
+#include "protocol.pb.h"
+#include "protocolmanager.h"
+
+#include <QCoreApplication>
+#include <QString>
+
+extern ProtocolManager *OstProtocolManager;
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication app(argc, argv);
+    QString error;
+
+    if (argc != 3)
+    {
+        printf("%s <infile> <outfile>\n", argv[0]);
+        exit(255);
+    }
+
+    OstProtocolManager = new ProtocolManager();
+
+    OstProto::StreamConfigList streams;
+    QString inFile(argv[1]);
+    QString outFile(argv[2]);
+
+    if (!pdmlFileFormat.openStreams(inFile, streams, error))
+    {
+        fprintf(stdout, "failed reading streams from %s\n", inFile.toAscii().constData());
+        exit(1);
+    }
+
+    if (!pcapFileFormat.saveStreams(streams, outFile, error))
+    {
+        fprintf(stdout, "failed writing streams to %s\n", outFile.toAscii().constData());
+        exit(1);
+    }
+
+    return 0;
+}
