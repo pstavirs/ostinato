763219e153d71e1d18a61c3e5dc5cebbbf4ff721
Srivats P. <pstavirs@gmail.com>
2011-10-23 17:39:15 +0530

    Integrated newer version (0.6.1) of the QHexEdit Widget from http://qhexedit2.googlecode.com. This newer version supports copy-paste and undo-redo.

--- extra/qhexedit2/src/qhexedit.h
+++ extra/qhexedit2/src/qhexedit.h
@@ -7,27 +7,40 @@
 /** \mainpage
 QHexEdit is a binary editor widget for Qt.
 
-\version Version 0.4.3
+\version Version 0.6.1
 \image html hexedit.png
 */
 
 
 /*! QHexEdit is a hex editor widget written in C++ for the Qt (Qt4) framework.
-It is a simple editor for binary data, just like QPlainTextEdit is for text data.
-There are sip configuration files included, so it is easy to create bindings
-for PyQt and you can use this widget also in python.
+It is a simple editor for binary data, just like QPlainTextEdit is for text
+data. There are sip configuration files included, so it is easy to create
+bindings for PyQt and you can use this widget also in python.
 
-QHexEdit takes the data of a QByteArray (setData()) and shows it. You can use the
-mouse or the keyboard to navigate inside the widget. If you hit the keys (0..9, a..f)
-you will change the data. Changed data is highlighted and can be accessed via data().
+QHexEdit takes the data of a QByteArray (setData()) and shows it. You can use
+the mouse or the keyboard to navigate inside the widget. If you hit the keys
+(0..9, a..f) you will change the data. Changed data is highlighted and can be
+accessed via data().
 
-Normaly QHexEdit works in the overwrite Mode. You can set overwriteMode(false) and
-insert data. In this case the size of data() increases. It is also possible to delete
-bytes under the cursor, here the size of data decreases.
+Normaly QHexEdit works in the overwrite Mode. You can set overwriteMode(false)
+and insert data. In this case the size of data() increases. It is also possible
+to delete bytes (del or backspace), here the size of data decreases.
 
-There are some limitations: The size of data has in general to be below 10 megabytes,
-otherwise the scroll sliders ard not shown and you can't scroll any more. Copy and
-paste functionality is perhaps a subject of a later release.
+You can select data with keyboard hits or mouse movements. The copy-key will
+copy the selected data into the clipboard. The cut-key copies also but delets
+it afterwards. In overwrite mode, the paste function overwrites the content of
+the (does not change the length) data. In insert mode, clipboard data will be
+inserted. The clipboard content is expected in ASCII Hex notation. Unknown
+characters will be ignored.
+
+QHexEdit comes with undo/redo functionality. All changes can be undone, by
+pressing the undo-key (usually ctr-z). They can also be redone afterwards.
+The undo/redo framework is cleared, when setData() sets up a new
+content for the editor.
+
+This widget can only handle small amounts of data. The size has to be below 10
+megabytes, otherwise the scroll sliders ard not shown and you can't scroll any
+more.
 */
         class QHexEdit : public QScrollArea
 {
@@ -55,10 +68,29 @@ paste functionality is perhaps a subject of a later release.
     */
     Q_PROPERTY(QColor highlightingColor READ highlightingColor WRITE setHighlightingColor)
 
+    /*! Property selection color sets (setSelectionColor()) the backgorund
+    color of selected text areas. You can also read the color
+    (selectionColor()).
+    */
+    Q_PROPERTY(QColor selectionColor READ selectionColor WRITE setSelectionColor)
+
     /*! Porperty overwrite mode sets (setOverwriteMode()) or gets (overwriteMode()) the mode
-    in which the editor works. In overwritem mode the user will overwrite existing data.
+    in which the editor works. In overwrite mode the user will overwrite existing data. The
+    size of data will be constant. In insert mode the size will grow, when inserting
+    new data.
     */
     Q_PROPERTY(bool overwriteMode READ overwriteMode WRITE setOverwriteMode)
+
+    /*! Porperty readOnly sets (setReadOnly()) or gets (isReadOnly) the mode
+    in which the editor works. In readonly mode the the user can only navigate
+    through the data and select data; modifying is not possible. This
+    property's default is false.
+    */
+    Q_PROPERTY(bool readOnly READ isReadOnly WRITE setReadOnly)
+
+    /*! Set the font of the widget. Please use fixed width fonts like Mono or Courier.*/
+    Q_PROPERTY(QFont font READ font WRITE setFont)
+
 
 public:
     /*! Creates an instance of QHexEdit.
@@ -69,23 +101,33 @@ public:
     /*! Inserts a byte array.
     \param i Index position, where to insert
     \param ba byte array, which is to insert
+    In overwrite mode, the existing data will be overwritten, in insertmode ba will be
+    insertet and size of data grows.
     */
     void insert(int i, const QByteArray & ba);
 
     /*! Inserts a char.
     \param i Index position, where to insert
     \param ch Char, which is to insert
+    In overwrite mode, the existing data will be overwritten, in insertmode ba will be
+    insertet and size of data grows.
     */
     void insert(int i, char ch);
 
     /*! Removes len bytes from the content.
     \param pos Index position, where to remove
     \param len Amount of bytes to remove
+    In overwrite mode, the existing bytes will be overwriten with 0x00.
     */
     void remove(int pos, int len=1);
 
-    /*! Set the font of the widget. Please use fixed width fonts like Mono or Courier.*/
-    void setFont(const QFont &);
+    /*! Gives back a formatted image of the content of QHexEdit
+    */
+    QString toReadableString();
+
+    /*! Gives back a formatted image of the selected content of QHexEdit
+    */
+    QString selectionToReadableString();
 
     /*! \cond docNever */
     void setAddressOffset(int offset);
@@ -96,11 +138,21 @@ public:
     QColor addressAreaColor();
     void setHighlightingColor(QColor const &color);
     QColor highlightingColor();
+    void setSelectionColor(QColor const &color);
+    QColor selectionColor();
     void setOverwriteMode(bool);
     bool overwriteMode();
+    void setReadOnly(bool);
+    bool isReadOnly();
+    const QFont &font() const;
+    void setFont(const QFont &);
     /*! \endcond docNever */
 
 public slots:
+    /*! Redoes the last operation. If there is no operation to redo, i.e.
+      there is no redo step in the undo/redo history, nothing happens.
+      */
+    void redo();
 
     /*! Set the minimum width of the address area.
       \param addressWidth Width in characters.
@@ -122,10 +174,18 @@ public slots:
       */
     void setHighlighting(bool mode);
 
+    /*! Undoes the last operation. If there is no operation to undo, i.e.
+      there is no undo step in the undo/redo history, nothing happens.
+      */
+    void undo();
+
 signals:
 
     /*! Contains the address, where the cursor is located. */
-    void currentAddress(int address);
+    void currentAddressChanged(int address);
+
+    /*! Contains the size of the data to edit. */
+    void currentSizeChanged(int size);
 
     /*! The signal is emited every time, the data is changed. */
     void dataChanged();
