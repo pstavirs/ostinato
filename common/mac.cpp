e05fa5a6902ecbc365e750f4f01964a150cb85d8
Srivats P <pstavirs@users.noreply.github.com>
2017-10-24 18:21:56 +0530

    UI improvements/fixes for Mac protocol
    * Change count/step to IntEdit with suitable minimums
    * Change mac address to MacEdit
    
    Fixes #224 fixes #188

--- common/mac.cpp
+++ common/mac.cpp
@@ -278,16 +278,14 @@ bool MacProtocol::setFieldData(int index, const QVariant &value,
     {
         case mac_dstAddr:
         {
-            quint64 mac = value.toString().toULongLong(&isOk, BASE_HEX);
-            if (isOk)
-                data.set_dst_mac(mac);
+            quint64 mac = value.toULongLong();
+            data.set_dst_mac(mac);
             break;
         }
         case mac_srcAddr:
         {
-            quint64 mac = value.toString().toULongLong(&isOk, BASE_HEX);
-            if (isOk)
-                data.set_src_mac(mac);
+            quint64 mac = value.toULongLong();
+            data.set_src_mac(mac);
             break;
         }
 
