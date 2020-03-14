7227dd734bd1b3c8b18f73bdd44b13dcf66b7556
Srivats P <pstavirs@users.noreply.github.com>
2020-03-14 21:42:29 +0530

    Fix build break due to missing include file

--- client/clipboardhelper.h
+++ client/clipboardhelper.h
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 #ifndef _CLIPBOARD_HELPER_H
 #define _CLIPBOARD_HELPER_H
 
+#include <QObject>
 #include <QList>
 
 class QAction;
