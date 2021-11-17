a1c165b7b315e90a330549eac8ac39b52a91e5ab
Srivats P <pstavirs@users.noreply.github.com>
2021-11-17 18:39:14 +0530

    Fix MacOS build break

--- client/portwidget.h
+++ client/portwidget.h
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 #define _PORT_WIDGET_H
 
 #include "ui_portwidget.h"
+#include <QModelIndex>
 #include <QWidget>
 
 class PortGroupList;
