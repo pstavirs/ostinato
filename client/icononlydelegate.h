ebe6aef62d785ddbae2e7cc4d094f5daf7df68d3
Srivats P <pstavirs@users.noreply.github.com>
2023-10-13 12:16:03 +0530

    Fix icon-only delegate
    The delegate was still rendering display text. But we didn't realize
    because the only place where this delegate was used was returning a
    QVariant() for DisplayText role earlier. Once we started returning a
    valid QString (for copy-paste purposes), this bug showed up.
    
    This didn't work because we further delegate the actual painting to
    the base QStyledItemDelegate which re-initializes option.features
    from the item index values, so removing HasDisplay does not work.
    
    The fix is to override the displayText() to return a null string.

--- client/icononlydelegate.h
+++ client/icononlydelegate.h
@@ -31,8 +31,12 @@ class IconOnlyDelegate : public QStyledItemDelegate
     {
         QStyleOptionViewItem opt = option;
         opt.decorationPosition = QStyleOptionViewItem::Top;
-        opt.features &= ~QStyleOptionViewItem::HasDisplay;
         QStyledItemDelegate::paint(painter, opt, index);
+    }
+
+    QString displayText(const QVariant&, const QLocale&) const
+    {
+        return QString();
     }
 };
 
